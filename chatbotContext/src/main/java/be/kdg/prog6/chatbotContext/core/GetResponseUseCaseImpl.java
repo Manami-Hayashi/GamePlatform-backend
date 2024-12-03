48174d867ccbac1f82fb6628af32a71158511a37
Manami Hayashi <manami.hayashi@student.kdg.be>
2024-12-03 08:38:07 +0100

    added GameRulesUseCase so it can retrieve GameRule from json file

--- chatbotContext/src/main/java/be/kdg/prog6/chatbotContext/core/GetResponseUseCaseImpl.java
+++ chatbotContext/src/main/java/be/kdg/prog6/chatbotContext/core/GetResponseUseCaseImpl.java
@@ -1,13 +1,27 @@
 package be.kdg.prog6.chatbotContext.core;
 
+import be.kdg.prog6.chatbotContext.ports.in.GetGameRulesUseCase;
 import be.kdg.prog6.chatbotContext.ports.in.GetResponseUseCase;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GetResponseUseCaseImpl implements GetResponseUseCase {
 
+    private final GetGameRulesUseCase getGameRulesUseCase;
+
+    public GetResponseUseCaseImpl(GetGameRulesUseCase getGameRulesUseCase) {
+        this.getGameRulesUseCase = getGameRulesUseCase;
+    }
+
+
     @Override
     public String getChatbotResponse(String userInput) {
+
+        if (userInput.toLowerCase().contains("rules")) {
+            // Extract game name from user input (e.g. "rules for Checkers")
+            String gameName = userInput.split("rules for")[-1].trim();
+            return getGameRulesUseCase.getGameRules(gameName);
+        }
         return "";
     }
 }
---
+++ chatbotContext/src/main/java/be/kdg/prog6/chatbotContext/ports/in/GetGameRulesUseCase.java
@@ -0,0 +1,5 @@
+package be.kdg.prog6.chatbotContext.ports.in;
+
+public interface GetGameRulesUseCase {
+    String getGameRules(String gameName);
+}
