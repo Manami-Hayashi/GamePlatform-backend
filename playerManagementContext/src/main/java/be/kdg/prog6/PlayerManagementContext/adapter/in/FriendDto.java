adddfad4b672815e9bbcd6b55d58a0c2542d3724
Noah Guerin <noah.guerin@student.kdg.be>
2024-12-12 16:43:53 +0100

    Complete friend requests endpoints

--- playerManagementContext/src/main/java/be/kdg/prog6/PlayerManagementContext/adapter/in/FriendDto.java
+++ playerManagementContext/src/main/java/be/kdg/prog6/PlayerManagementContext/adapter/in/FriendDto.java
@@ -1,25 +1,9 @@
 package be.kdg.prog6.PlayerManagementContext.adapter.in;
 
-public class FriendDto {
-    private final String playerId;
-    private final String name;
-    private final String friendRequestStatus;
-
-    public FriendDto(String playerId, String name, String friendRequestStatus) {
-        this.playerId = playerId;
-        this.name = name;
-        this.friendRequestStatus = friendRequestStatus;
-    }
-
-    public String getPlayerId() {
-        return playerId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getFriendRequestStatus() {
-        return friendRequestStatus;
-    }
+public record FriendDto(
+        String senderId,
+        String senderName,
+        String receiverId,
+        String receiverName,
+        String friendRequestStatus) {
 }
